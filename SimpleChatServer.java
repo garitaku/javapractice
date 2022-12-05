5026d0332f5da7f96513f6f268f49a8829a729b7
garitaku <107231507+garitaku@users.noreply.github.com>
2022-12-05 17:08:38 +0900

    Add files via upload

---
+++ SimpleChatServer.java
@@ -0,0 +1,70 @@
+package network;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SimpleChatServer {
+	public static void main(String[] args) {
+		try {
+			System.out.println("サーバー起動ちう");
+			ServerSocket ss = new ServerSocket(5000);
+			while (true) {
+				Socket cs = ss.accept();
+				ChatThread chat = new ChatThread(cs);
+				chat.start();
+
+//				BufferedReader br = new BufferedReader(new InputStreamReader(cs.getInputStream()));
+//				
+//				String message = br.readLine();
+//				System.out.println(message + "を受け取りました");
+//
+////				BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(cs.getOutputStream()));
+//				PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(cs.getOutputStream())));
+//				pw.println(message);
+////				pw.flush();
+//				pw.close();
+//				br.close();
+			}
+		} catch (Exception e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+	}
+}
+
+class ChatThread extends Thread {
+
+	private Socket cs;
+
+	public ChatThread(Socket cs) {
+		this.cs = cs;
+	}
+
+	public void run() {
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new InputStreamReader(cs.getInputStream()));
+			String message = "メッセージはありません。";
+			message = br.readLine();
+			System.out.println(message + "を受け取りました");
+			PrintWriter pw;
+			pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(cs.getOutputStream())));
+			pw.println(message);
+//			pw.flush();
+			pw.close();
+			br.close();
+
+		} catch (IOException e1) {
+			// TODO 自動生成された catch ブロック
+			e1.printStackTrace();
+		}
+
+	}
+
+}
